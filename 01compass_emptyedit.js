102fb1a42190b9b794650c17d7e06fbd34cfaf8d
Sienna Broglie <Sienna@Siennas-MacBook-Pro.local>
2018-02-13 14:15:17 -0600

    Initial commit

---
+++ 01compass_emptyedit.js
@@ -0,0 +1,37 @@
+//called by device orientation listener - contains information about the change in orientation (stored as argument "e")
+oriented = function(e) {
+        heading = e.webkitCompassHeading;
+        alpha = e.alpha;
+        if(e.webkitCompassHeading) {
+            compassHeading = e.webkitCompassHeading;
+        }   else  { 
+            compassHeading = e.alpha;
+        }
+        
+        //additions
+        //|| or, && and, == 
+        if (compassHeading > 315 || compassHeading < 45)
+             text_area.textContent = compassHeading;
+
+
+         
+        //calls function to alter content based on heading
+       // myOrientation();
+    }
+
+//device orientation that can track orientation on space
+	window.addEventListener('deviceorientation', oriented, false);
+    
+
+
+//find the tag on the html page to put text into
+var text_area = document.getElementById("compass_content");
+
+
+    function myOrientation(){
+     
+            text_area.textContent = "empty compass";
+        
+	}
+
+
