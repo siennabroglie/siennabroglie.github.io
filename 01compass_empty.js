30b2a8e8bfd9f7ed7a642511ddad01cc94d26825
Sienna Broglie <Sienna@Siennas-MacBook-Pro.local>
2018-02-13 14:30:37 -0600

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -11,19 +11,19 @@ oriented = function(e) {
         //additions
         //|| or, && and, == 
         if (compassHeading >= 315 || compassHeading <= 45)
-             text_area.textContent = compassHeading;
+            {text_area.textContent = north;}
 
 
         if (compassHeading >= 45 || compassHeading <= 135)
-             text_area.textContent = compassHeading;
+             {text_area.textContent = east;}
 
 
          if (compassHeading >= 135 || compassHeading <= 225)
-             text_area.textContent = compassHeading;
+             {text_area.textContent = south;}
          
 
           if (compassHeading >= 225 || compassHeading <= 315)
-             text_area.textContent = compassHeading;
+             {text_area.textContent = west;}
 
 
         //calls function to alter content based on heading
