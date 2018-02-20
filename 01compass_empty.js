a19f893218b33f24bc11cc453e083584aaa46845
siennabroglie <Sienna@Siennas-MacBook-Pro.local>
2018-02-20 11:02:20 -0600

    justwork

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -17,19 +17,19 @@ oriented = function(e) {
 
 
         if (compassHeading >= 315 || compassHeading <= 45)
-            {text_area.textContent = compassHeading && "22, you are frozen";}
+            {text_area.textContent = "22 \n you are frozen";}
 
 
         if (compassHeading >= 45 && compassHeading <= 135)
-             {text_area.textContent = "110, you are still";}
+             {text_area.textContent = "110 \n you are still";}
 
 
         if (compassHeading >= 135 && compassHeading <= 225)
-             {text_area.textContent = "222, you are burning";}
+             {text_area.textContent = "222 \n you are burning";}
          
 
         if (compassHeading >= 225 && compassHeading <= 315)
-             {text_area.textContent = "300, you are blowing";}
+             {text_area.textContent = "300 \n you are blowing";}
 
 
         //calls function to alter content based on heading
