0248813cc5aaab2d2282bef88019e89097b218e4
siennabroglie <Sienna@Siennas-MacBook-Pro.local>
2018-03-26 23:30:55 -0500

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -17,19 +17,19 @@ oriented = function(e) {
 
 
         if (compassHeading >= 315 || compassHeading <= 45)
-            {text_area.textContent = " NORTH \n - \n does not really exist \n eather does not exist, electromagnetic waves have no medium \n the speed of light is the same to all viewers and not relative ";}
+            {text_area.textContent = " NORTH | does not really exist | eather does not exist, electromagnetic waves have no medium | the speed of light is the same to all viewers and not relative ";}
 
 
         if (compassHeading >= 45 && compassHeading <= 135)
-             {text_area.textContent = " EAST \n - \n does not really exist \n light has a fixed constant speed independent of any state of motion \n the principle of invarient light speed ";}
+             {text_area.textContent = " EAST | does not really exist | light has a fixed constant speed independent of any state of motion | the principle of invarient light speed ";}
 
 
         if (compassHeading >= 135 && compassHeading <= 225)
-             {text_area.textContent = " SOUTH \n - \n does not really exist \n physical laws are independent of the inertial system \n the principle of relativity ";}
+             {text_area.textContent = " SOUTH | does not really exist | physical laws are independent of the inertial system | the principle of relativity ";}
          
 
         if (compassHeading >= 225 && compassHeading <= 315)
-             {text_area.textContent = " WEST \n - \n does not really exist \n there is no way to measure a[the] fixed central point of our universe \n the principle of special relativity ";}
+             {text_area.textContent = " WEST | does not really exist | there is no way to measure a[the] fixed central point of our universe | the principle of special relativity ";}
 
 
         //calls function to alter content based on heading
